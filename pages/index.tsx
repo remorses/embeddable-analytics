5c148cccba71fa5af586cbce83907b3373565eba
remorses <beats.by.morse@gmail.com>
2024-01-28 19:08:28 +0100

    added tracker code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,20 @@
 import Script from 'next/script'
 import Meta from '../components/Meta'
 import Widgets from '../components/Widgets'
+import { track } from '../tracker'
+import { useEffect } from 'react'
 
+const token =
+  'p.eyJ1IjogIjJlNDUyMzlmLTliZDItNGE5YS1iMDkwLTJmNjMyY2EwODJkZiIsICJpZCI6ICJmNTI0NTliMi1hOGZiLTQ4ZTEtODNiYi1kMzMyMjgwM2JjMjEiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.NZPo51CYeFx7PLLEnGK6vIfN4z1SMnED4oyosvbRiMg'
 export default function DashboardPage() {
   const namespace = 'tinybird'
   const domain = 'tinybird.co'
+
+  useEffect(() => {
+    track({ namespace: 'x', token })
+  }, [])
   return (
     <>
-      {process.env.NODE_ENV === 'production' && (
-        <Script
-          defer
-          src="https://unpkg.com/@tinybirdco/flock.js"
-          data-token={process.env.NEXT_PUBLIC_TINYBIRD_TRACKER_TOKEN}
-        />
-      )}
       <Meta />
       <div className="bg-body min-h-screen py-5 px-5 sm:px-10 text-sm leading-5 text-secondary">
         <div className="max-w-7xl mx-auto">

dc4beae6ca9629c013621f7e5fb77f6595631f65
remorses <beats.by.morse@gmail.com>
2024-01-28 18:28:45 +0100

    server: refactor

--- server/route.ts
+++ server/route.ts
@@ -1,4 +1,4 @@
-import { fetchTinyBird } from './api'
+import { ClientResponse, QueryError } from '../lib/types'
 
 export async function getData({
   pipe,
@@ -27,4 +27,37 @@ export async function getData({
   })
 
   return res
+}
+
+async function fetchTinyBird<T>({
+  host,
+  token,
+  params,
+  path,
+}: {
+  path: string
+  params?: RequestInit
+  host
+  token
+}): Promise<ClientResponse<T>> {
+  if (!token || !host) throw new Error('Configuration not found')
+
+  const apiUrl =
+    {
+      'https://ui.tinybird.co': 'https://api.tinybird.co',
+      'https://ui.us-east.tinybird.co': 'https://api.us-east.tinybird.co',
+    }[host] ?? host
+
+  const response = await fetch(`${apiUrl}/v0${path}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    ...params,
+  })
+  const data = (await response.json()) as ClientResponse<T>
+
+  if (!response.ok) {
+    throw new QueryError(data?.error ?? 'Something went wrong', response.status)
+  }
+  return data
 }

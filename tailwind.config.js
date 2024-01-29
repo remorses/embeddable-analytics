b54542ad103ad7a1608bc20feac4b81377456918
remorses <beats.by.morse@gmail.com>
2024-01-29 11:21:52 +0100

    removed non default tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -113,10 +113,5 @@ module.exports = {
         /^(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))$/,
     },
   ],
-  plugins: [
-    function ({ addVariant }) {
-      addVariant('state-active', '&[data-state="active"]')
-    },
-    require('@headlessui/tailwindcss'),
-  ],
+  plugins: [require('@headlessui/tailwindcss')],
 }

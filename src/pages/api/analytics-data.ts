f78532907114b085155921e442196e504773aacd
Tommy D. Rossi <beats.by.morse@gmail.com>
2025-11-04 20:44:31 +0100

    nn

--- src/pages/api/analytics-data.ts
+++ src/pages/api/analytics-data.ts
@@ -1,17 +1,20 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-import { getData } from '../server/route'
+import { getData } from '../../server/route'
 
 const token = `p.eyJ1IjogIjJlNDUyMzlmLTliZDItNGE5YS1iMDkwLTJmNjMyY2EwODJkZiIsICJpZCI6ICIxMzJjNjU4Ny00MzcxLTRmMzUtYTM4Mi02NzUxMzk4Zjc3YTIiLCAiaG9zdCI6ICJldV9zaGFyZWQifQ.pHGknDiZ8BbNK-IZ_XqwUjCg6NsR5JuNov_QYmYveXs`
 
-export default async function route(req: NextRequest) {
-  const json = await req.json()
-  // console.log(json)
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method Not Allowed' })
+    return
+  }
+
+  const json = req.body
   const namespace: string = json.namespace
   // do auth here
   const data = await getData({ ...json, token, namespace })
-//   console.log(data)
-  return NextResponse.json(data)
+  res.status(200).json(data)
 }
-
-export const runtime = 'edge'
